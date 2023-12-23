f6d0c7125a825afadf9b516641756982b7615de5
gportodev <gportodev@gmail.com>
2023-12-23 10:56:45 -0300

    Add sublist

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -6,7 +6,30 @@ import { Circle, Divider } from '../../assets/Loader';
 import Colors from '../../constants/Colors';
 
 function List(): JSX.Element {
-  const data = [
+  // const data = [
+  //   {
+  //     time: '20:00',
+  //     description: 'Xtudo',
+  //     isPartOfDiet: false,
+  //   },
+  //   {
+  //     time: '16:00',
+  //     description: 'Whey',
+  //     isPartOfDiet: true,
+  //   },
+  //   {
+  //     time: '12:30',
+  //     description: 'Salada cesar com frango',
+  //     isPartOfDiet: true,
+  //   },
+  //   {
+  //     time: '09:30',
+  //     description: 'Vitamina de banana',
+  //     isPartOfDiet: true,
+  //   },
+  // ];
+
+  const arr = [
     {
       time: '20:00',
       description: 'Xtudo',
@@ -29,47 +52,71 @@ function List(): JSX.Element {
     },
   ];
 
+  const data = [
+    {
+      day: '12.08.22',
+      meals: arr,
+    },
+    {
+      day: '13.08.22',
+      meals: arr,
+    },
+  ];
+
   return (
     <>
-      <View
-        style={{
-          paddingTop: 32,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 18,
-            fontFamily: Fonts.bold,
-            lineHeight: 23.4,
-          }}
-        >
-          12.08.22
-        </Text>
-      </View>
-
       <FlatList
         data={data}
-        contentContainerStyle={{
-          paddingTop: 8,
-        }}
+        scrollEnabled={false}
         renderItem={({ item }) => (
-          <View style={styles.container}>
-            <View style={styles.wrap}>
-              <Text style={styles.timeText}>{item.time}</Text>
-
-              <Divider style={styles.divider} />
-
-              <Text style={styles.descriptionText}>{item.description}</Text>
+          <>
+            <View
+              style={{
+                paddingTop: 32,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: Fonts.bold,
+                  lineHeight: 23.4,
+                }}
+              >
+                {item.day}
+              </Text>
             </View>
 
-            <Circle
-              width={14}
-              height={14}
-              color={
-                item.isPartOfDiet ? Colors.greens.greensMid : Colors.reds.redMid
-              }
+            <FlatList
+              data={item.meals}
+              scrollEnabled={false}
+              contentContainerStyle={{
+                paddingTop: 8,
+              }}
+              renderItem={({ item }) => (
+                <View style={styles.container}>
+                  <View style={styles.wrap}>
+                    <Text style={styles.timeText}>{item.time}</Text>
+
+                    <Divider style={styles.divider} />
+
+                    <Text style={styles.descriptionText}>
+                      {item.description}
+                    </Text>
+                  </View>
+
+                  <Circle
+                    width={14}
+                    height={14}
+                    color={
+                      item.isPartOfDiet
+                        ? Colors.greens.greensMid
+                        : Colors.reds.redMid
+                    }
+                  />
+                </View>
+              )}
             />
-          </View>
+          </>
         )}
       />
     </>
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Button } from '../../components/Button';
 import { PencilSimpleLine, Plus } from 'phosphor-react-native';
 import Colors from '../../constants/Colors';
@@ -14,89 +14,90 @@ import { List } from '../../components/List';
 
 function Home(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Header />
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        <Header />
 
-      <Card
-        number={'90.86'}
-        text={'das refeições dentro da dieta'}
-        percent
-        icon
-        containerStyle={{
-          marginTop: 30,
-          height: 102,
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingTop: 20,
-          paddingBottom: 20,
-          paddingRight: 16,
-          paddingLeft: 16,
-          gap: 2,
-          backgroundColor: Colors.greens.greensLight,
-          borderRadius: 8,
-        }}
-        numberStyle={{
-          color: Colors.grays.gray1,
-          fontFamily: Fonts.bold,
-          fontSize: 32,
-          lineHeight: 41.6,
-        }}
-        textStyle={{
-          color: Colors.grays.gray1,
-          fontFamily: Fonts.regular,
-          fontSize: 14,
-          lineHeight: 18.2,
-        }}
-      />
-
-      <View
-        style={{
-          paddingTop: 40,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 16,
-            lineHeight: 20.8,
-            fontFamily: Fonts.regular,
-            color: Colors.grays.gray1,
-            paddingBottom: 8,
-          }}
-        >
-          Refeições
-        </Text>
-
-        <Button
-          title="Nova refeição"
-          titleStyle={{
-            fontSize: 14,
-            lineHeight: 18.2,
-            fontFamily: Fonts.bold,
-            color: Colors.white,
-          }}
-          style={{
+        <Card
+          number={'90.86'}
+          text={'das refeições dentro da dieta'}
+          percent
+          icon
+          containerStyle={{
+            marginTop: 30,
+            height: 102,
             width: '100%',
-            height: 50,
-            paddingVertical: 16,
-            paddingHorizontal: 24,
             justifyContent: 'center',
             alignItems: 'center',
-            gap: 12,
-            borderWidth: 1,
-            borderStyle: 'solid',
-            borderColor: Colors.grays.gray1,
-            borderRadius: 6,
-            backgroundColor: Colors.grays.gray2,
+            paddingTop: 20,
+            paddingBottom: 20,
+            paddingRight: 16,
+            paddingLeft: 16,
+            gap: 2,
+            backgroundColor: Colors.greens.greensLight,
+            borderRadius: 8,
+          }}
+          numberStyle={{
+            color: Colors.grays.gray1,
+            fontFamily: Fonts.bold,
+            fontSize: 32,
+            lineHeight: 41.6,
+          }}
+          textStyle={{
+            color: Colors.grays.gray1,
+            fontFamily: Fonts.regular,
+            fontSize: 14,
+            lineHeight: 18.2,
+          }}
+        />
+
+        <View
+          style={{
+            paddingTop: 40,
           }}
         >
-          <Plus color={Colors.white} size={18} />
-        </Button>
-      </View>
+          <Text
+            style={{
+              fontSize: 16,
+              lineHeight: 20.8,
+              fontFamily: Fonts.regular,
+              color: Colors.grays.gray1,
+              paddingBottom: 8,
+            }}
+          >
+            Refeições
+          </Text>
 
-      <List />
+          <Button
+            title="Nova refeição"
+            titleStyle={{
+              fontSize: 14,
+              lineHeight: 18.2,
+              fontFamily: Fonts.bold,
+              color: Colors.white,
+            }}
+            style={{
+              width: '100%',
+              height: 50,
+              paddingVertical: 16,
+              paddingHorizontal: 24,
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: 12,
+              borderWidth: 1,
+              borderStyle: 'solid',
+              borderColor: Colors.grays.gray1,
+              borderRadius: 6,
+              backgroundColor: Colors.grays.gray2,
+            }}
+          >
+            <Plus color={Colors.white} size={18} />
+          </Button>
+        </View>
 
-      {/* <Button
+        <List />
+
+        {/* <Button
         title="Label"
         titleStyle={{
           fontSize: 14,
@@ -173,7 +174,8 @@ function Home(): JSX.Element {
           gap: 8,
         }}
       /> */}
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
